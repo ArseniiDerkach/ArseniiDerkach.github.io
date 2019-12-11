2893959ca1d3ded7dc48fc4c93226bc73c8ae9c5
Arsenii Derkach <arsenii.derkach@revjet.com>
2019-12-11 20:42:02 +0200

    added mobile responsive to home page

--- js/script.js
+++ js/script.js
@@ -10,6 +10,21 @@ $(document).ready(function(){
         slidesToShow: 3,
         arrow: true,
         prevArrow: $('.about-us__slider-prev'),
-        nextArrow: $('.about-us__slider-next')
+        nextArrow: $('.about-us__slider-next'),
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                arrow: false
+              }
+            }
+          ]
       });
   });
