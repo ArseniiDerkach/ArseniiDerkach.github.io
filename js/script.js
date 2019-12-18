37625274e52c097003a3d16348690d8cde839aa1
Arsenii Derkach <arsenii.derkach@revjet.com>
2019-12-18 13:12:10 +0100

    robo-t

--- js/script.js
+++ js/script.js
@@ -1,30 +1,5 @@
 $(document).ready(function(){
-    $('.info__slider').slick({
-        infinite: true,
-        dots: true,
-        arrows: false,
-      });
-    
-    $('.about-us__slider').slick({
-        infinite: true,
-        slidesToShow: 3,
-        arrow: true,
-        prevArrow: $('.about-us__slider-prev'),
-        nextArrow: $('.about-us__slider-next'),
-        responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 2
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                arrow: false
-              }
-            }
-          ]
-      });
-  });
+    $('.graph-slider').slick({
+        dots: true
+    });
+})
