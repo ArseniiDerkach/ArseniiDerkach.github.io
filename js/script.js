c2d257d97c8cfdff4d9935acdaea9fc408d3007d
Arsenii Derkach <arsenii.derkach@revjet.com>
2019-12-19 12:19:49 +0100

    fixed mobile view

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
     $('.graph-slider').slick({
-        dots: true
+        dots: true,
+        slidesToShow: 2,
+        responsive: [
+            {
+              breakpoint: 991,
+              settings: {
+                slidesToShow: 1
+              }
+            }
+        ]
     });
 })
