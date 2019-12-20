f7f8fa3fa5e14fa2fbc47b7abadc563745868b31
Arsenii Derkach <arsenii.derkach@revjet.com>
2019-12-20 01:19:15 +0100

    added header burger menu, added text animations

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+    setTimeout(function(){
+      $(".preloader").removeClass('preloader');
+    },400);
+    setTimeout(()=>{
+      $('.promo__sign-up-heading.loading').removeClass('loading');
+      setTimeout(()=>{
+        $('.promo__form .email').removeClass('loading');
+        setTimeout(()=>{
+          $('.promo__form .btn').removeClass('loading');
+        },1000)
+      },1000)
+    },2000)
     $('.graph-slider').slick({
         dots: true,
         slidesToShow: 2,
@@ -11,4 +23,23 @@ $(document).ready(function(){
             }
         ]
     });
+    window.addEventListener('scroll',()=>{
+      const windowHeight = window.innerHeight;
+      [...$('.animated-headline')].forEach((item,index)=>{
+        const top = item.getBoundingClientRect().top;
+        console.log(`${index}: ${top}`);
+        if (top < windowHeight) {
+          const diff = windowHeight - top;
+          const progress = diff*2/windowHeight;
+          const percent = progress > 1 ? 1 : progress;
+          item.style.transform = `translateY(${100 - percent*100}px)`;
+          item.style.opacity = `${percent}`;
+        }
+      })
+      console.log(window.innerHeight);
+    })
+    $('.toggle-menu').click(function(){
+      $(this).toggleClass('active');
+      $('.header__menu').toggleClass('active');
+    })
 })
