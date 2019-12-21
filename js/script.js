6d3ddbc1555566cf8f60e14616d8301954c149d4
Arsenii Derkach <arsenii.derkach@revjet.com>
2019-12-21 05:40:08 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -39,31 +39,41 @@ $(document).ready(function(){
 
 
       if ($('.statistics')[0].getBoundingClientRect().top < windowHeight) {
+        console.log($('.statistics')[0].getBoundingClientRect().top);
         [...$(".statistic__item")].forEach((item,index)=>{
           setTimeout(()=>{
             item.classList.remove('hidden');
           },(index+1)*500);
         })
+        const triangleSm = $('.statistics .triangle-sm');
+        const triangleLg = $('.statistics .triangle-lg');
+        const offsetTop = windowHeight - $('.statistics')[0].getBoundingClientRect().top + windowHeight;
+        const topSm = triangleSm.css('top');
+        const topLg = triangleLg.css('top');
+        console.log(topSm,topLg);
+        const scrollProgress = offsetTop / (2 * windowHeight);
+        triangleSm.css('top',`${windowHeight - scrollProgress * windowHeight/2}px`);
+        triangleSm.css('bottom',`unset`);
+        triangleLg.css('top',`${windowHeight - scrollProgress * windowHeight/2}px`);
+        triangleLg.css('bottom',`unset`);
       }
 
-      if (($('.history-diagram')[0].getBoundingClientRect().top < windowHeight) && (!counter)) {
+      if (($('.history-diagram-item')[0].getBoundingClientRect().top < windowHeight) && (!counter)) {
         counter = true
         $('.countable').each(function () {
           var $this = $(this);
-          // str.match(/.{1,3}/g)
           if ($this.hasClass('volume')) {
             jQuery({ Counter: 0 }).animate({ Counter: ($this.text().slice(1).split(' ').join('')) }, {
-              duration: 1000,
+              duration: 2000,
               easing: 'swing',
               step: function () {
-                console.log((Math.ceil(this.Counter)+'').match(/.{1,3}/g).join(' '));
                 $this.text(`$${(Math.ceil(this.Counter)+'').toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}`);
               }
             });
           } else
           if ($this.hasClass('transactions')) {
             jQuery({ Counter: 0 }).animate({ Counter: ($this.text().split(' ').join('')) }, {
-              duration: 1000,
+              duration: 2000,
               easing: 'swing',
               step: function () {
                 $this.text(`${(Math.ceil(this.Counter)+'').toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}`);
@@ -72,7 +82,7 @@ $(document).ready(function(){
           } else
           if ($this.hasClass('bankroll-value')) {
             jQuery({ Counter: 0 }).animate({ Counter: (parseInt($this.text())) }, {
-              duration: 1000,
+              duration: 2000,
               easing: 'swing',
               step: function () {
                 $this.text(`${(Math.ceil(this.Counter))}%`);
@@ -81,7 +91,7 @@ $(document).ready(function(){
           }
           else {
             jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-              duration: 1000,
+              duration: 2000,
               easing: 'swing',
               step: function () {
                 $this.text(Math.ceil(this.Counter));
@@ -91,8 +101,73 @@ $(document).ready(function(){
           
         });
       }
+
+      if ($('.register')[0].getBoundingClientRect().top < windowHeight) {
+        const triangleSm = $('.register .triangle-sm');
+        const triangleLg = $('.register .triangle-lg');
+        const offsetTop = windowHeight - $('.register')[0].getBoundingClientRect().top + windowHeight;
+        const topSm = triangleSm.css('top');
+        const topLg = triangleLg.css('top');
+        console.log(topSm,topLg);
+        const scrollProgress = offsetTop / (2 * windowHeight);
+        triangleSm.css('top',`${windowHeight + scrollProgress * windowHeight/2}px`);
+        triangleSm.css('bottom',`unset`);
+        triangleLg.css('top',`${windowHeight + scrollProgress * windowHeight/2}px`);
+        triangleLg.css('bottom',`unset`);
+       }
       
     })
+  
+
+    function runRobot() {
+      const teeth = [...$('.robot-tooth')];
+      const heights = teeth.map(item=>$(item).data('tooth-height'));
+      teeth.forEach((item,i)=>{
+        const random = Math.random()*10 - 5;
+        $(item).height(heights[i]+random);
+      })
+    }
+    const placeholderTableData = [
+      {
+        value: 120,
+        date: '10:15 at 20/10/2019'
+      },
+      {
+        value: 1000,
+        date: '10:16 at 20/10/2019'
+      },
+      {
+        value: 2000,
+        date: '10:18 at 20/10/2019'
+      },
+      {
+        value: 100,
+        date: '10:20 at 20/10/2019'
+      },
+      {
+        value: 250,
+        date: '10:22 at 20/10/2019'
+      },
+      {
+        value: 500,
+        date: '10:24 at 20/10/2019'
+      },
+    ]
+    function updateTable(type) {
+      debugger;
+      // deposit-history
+      // cashout-history
+      const list = $(`.${type}-history`);
+      const items = [...list[0].querySelectorAll('.history__list-item')];
+      const lastItem = items.pop();
+      const newItem = lastItem.cloneNode(true);
+      const newItemData = placeholderTableData.shift();
+      newItem.querySelector('.history__item-value').innerText = `$${newItemData.value}`;
+      newItem.querySelector('.history__item-date').innerText = `$${newItemData.date}`;
+      items.unshift(newItem);
+    }
+    updateTable('deposit');
+    setInterval(runRobot,100);
     $('.toggle-menu').click(function(){
       $(this).toggleClass('active');
       $('.header__menu').toggleClass('active');
